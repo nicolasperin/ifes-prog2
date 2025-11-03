1aec3645add9ebfecbbef52fccb1a88d6c70088e
nicolasperin <nicopperin@gmail.com>
2025-11-03 15:16:01 -0300

    lista 9

--- lista-de-exercicios-09/exercicio-03/exercicio-03.c
+++ lista-de-exercicios-09/exercicio-03/exercicio-03.c
@@ -1,26 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define MAX_ALUNOS 100
-#define MAX_PROJETOS 3
 #define TAM_NOME 20
 
-typedef struct {
-    int matricula;
-    char projetos[MAX_PROJETOS][TAM_NOME];
-} Aluno;
+FILE *abrirArquivo(const char *nome_arquivo, const char *modo) {
+    FILE *arquivo = fopen(nome_arquivo, modo);
 
+    if (arquivo == NULL) {
+        fprintf(stderr, "Erro ao abrir o arquivo: %s\n", nome_arquivo);
+        exit(EXIT_FAILURE);
+    }
 
-int main(){
+    return arquivo;
+}
 
-    FILE *arquivo_input, *arquivo_output; 
-
-    Aluno alunos[MAX_ALUNOS];
-
+int ehNumero(const char *str) {
     int i;
+    for (i = 0; i < strlen(str); i++) {
+        if (!isdigit(str[i])) {
+            return 0; 
+        }
+    }
+    return 1;
+}
 
-    arquivo_input = fopen("ex3_alunos.txt", "r");
+int main(void) {
+    FILE *arquivo_input = abrirArquivo("../ex3_alunos.txt", "r");
+    FILE *arquivo_leds = abrirArquivo("leds.txt", "w");
+    FILE *arquivo_leter = abrirArquivo("leter.txt", "w");
+    FILE *arquivo_robotica = abrirArquivo("robotica.txt", "w");
+
+    char palavra[TAM_NOME];
+    int matricula = 0;
+
+    while (fscanf(arquivo_input, "%s", palavra) == 1) {
+
+        if (ehNumero(palavra)) {
+            matricula = atoi(palavra); 
+        }
+
+        if (strcmp(palavra, "LETER") == 0) {
+            fprintf(arquivo_leter, "%d\n", matricula);
+        } 
+        else if (strcmp(palavra, "LEDS") == 0) {
+            fprintf(arquivo_leds, "%d\n", matricula);
+        } 
+        else if (strcmp(palavra, "Robótica") == 0) {
+            fprintf(arquivo_robotica, "%d\n", matricula);
+        }
+    }
+
+    fclose(arquivo_input);
+    fclose(arquivo_leds);
+    fclose(arquivo_leter);
+    fclose(arquivo_robotica);
 
     return 0;
-
 }
